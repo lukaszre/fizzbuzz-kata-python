117045febb180fe27e552b2da5e319db0e8b78cf
lreszk01 <lukasz.reszka@orange.com>
2020-06-16 14:49:05 +0200

    fizzbuzzGenerator and program is ready

--- fizzbuzz/FizzBuzz.py
+++ fizzbuzz/FizzBuzz.py
@@ -11,6 +11,24 @@ class FizzBuzz:
 
         return str(number)
 
+    def fizzBuzzGenerator(self, number):
+        if number <= 0:
+            raise InvalidNumberException
+        result = ""
+        for i in range(number):
+            result += self.fizzBuzz(i+1) + ","
+
+        return result[:-1]
+
 
 class InvalidNumberException(Exception):
     pass
+
+
+def main():
+    fizzbuzz = FizzBuzz()
+    print(fizzbuzz.fizzBuzzGenerator(100))
+
+
+if __name__ == '__main__':
+    main()
--- fizzbuzz/FizzBuzzTest.py
+++ fizzbuzz/FizzBuzzTest.py
@@ -18,46 +18,69 @@ class FizzBuzzTest(unittest.TestCase):
     def test_should_return_number_when_1(self):
         fizzbuzz_result = self.fizzbuzz.fizzBuzz(number=1)
 
-        self.assertEqual(fizzbuzz_result, "1")
+        self.assertEqual("1", fizzbuzz_result)
 
     def test_should_return_number_when_2(self):
         fizzbuzz_result = self.fizzbuzz.fizzBuzz(number=2)
 
-        self.assertEqual(fizzbuzz_result, "2")
+        self.assertEqual("2", fizzbuzz_result)
 
     def test_should_return_fizz_when_3(self):
         fizzbuzz_result = self.fizzbuzz.fizzBuzz(number=3)
 
-        self.assertEqual(fizzbuzz_result, FIZZ)
+        self.assertEqual(FIZZ, fizzbuzz_result)
 
     def test_should_return_buzz_when_5(self):
         fizzbuzz_result = self.fizzbuzz.fizzBuzz(number=5)
 
-        self.assertEqual(fizzbuzz_result, BUZZ)
+        self.assertEqual(BUZZ, fizzbuzz_result)
 
     def test_should_return_fizz_when_9(self):
         fizzbuzz_result = self.fizzbuzz.fizzBuzz(number=9)
 
-        self.assertEqual(fizzbuzz_result, FIZZ)
+        self.assertEqual(FIZZ, fizzbuzz_result)
 
     def test_should_return_buzz_when_10(self):
         fizzbuzz_result = self.fizzbuzz.fizzBuzz(number=10)
 
-        self.assertEqual(fizzbuzz_result, BUZZ)
+        self.assertEqual(BUZZ, fizzbuzz_result)
 
     def test_should_return_fizzbuzz_when_15(self):
         fizzbuzz_result = self.fizzbuzz.fizzBuzz(number=15)
 
-        self.assertEqual(fizzbuzz_result, FIZZBUZZ)
+        self.assertEqual(FIZZBUZZ, fizzbuzz_result)
 
     def test_should_return_fizzbuzz_when_45(self):
         fizzbuzz_result = self.fizzbuzz.fizzBuzz(number=45)
 
-        self.assertEqual(fizzbuzz_result, FIZZBUZZ)
+        self.assertEqual(FIZZBUZZ, fizzbuzz_result)
 
     def test_should_throw_Exception_when_0(self):
         with self.assertRaises(InvalidNumberException):
             self.fizzbuzz.fizzBuzz(number=0)
+
+
+class FizzBuzzGeneratorTest(unittest.TestCase):
+    fizzbuzz = FizzBuzz()
+
+    def test_should_return_1_when_1(self):
+        fizzbuzz_result = self.fizzbuzz.fizzBuzzGenerator(number=1)
+
+        self.assertEqual("1", fizzbuzz_result)
+
+    def test_should_return_1comma2_when_2(self):
+        fizzbuzz_result = self.fizzbuzz.fizzBuzzGenerator(number=2)
+
+        self.assertEqual("1,2", fizzbuzz_result)
+
+    def test_should_throw_Exception_when_0(self):
+        with self.assertRaises(InvalidNumberException):
+            self.fizzbuzz.fizzBuzzGenerator(number=0)
+
+    def test_should_return_1comma2commafizz_when_3(self):
+        fizzbuzz_result = self.fizzbuzz.fizzBuzzGenerator(number=3)
+
+        self.assertEqual("1,2,fizz", fizzbuzz_result)
 
 
 if __name__ == '__main__':
